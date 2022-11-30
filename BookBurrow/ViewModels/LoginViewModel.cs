f0cf779fa11fac451b78832ffa0c5133d54b9d74
Sarah Maginnis <sarahemaginnis@gmail.com>
2022-11-29 21:24:34 -0600

    first draft of view models

---
+++ BookBurrow/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System.ComponentModel.DataAnnotations;
+using BookBurrow.Models;
+
+namespace BookBurrow.ViewModels
+{
+    public class LoginViewModel
+    {
+        public User User { get; set; }
+        public UserProfile UserProfile { get; set; }
+    }
+}
