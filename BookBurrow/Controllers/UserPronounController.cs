2c15d04828888c6febc9e65687d1d79eaf30292a
Sarah Maginnis <sarahemaginnis@gmail.com>
2022-12-03 16:16:31 -0600

    created repository, interface, and controller for UserPronoun to setup basic CRUD functionality

---
+++ BookBurrow/Controllers/UserPronounController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using BookBurrow.Repositories;
+using BookBurrow.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookBurrow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserPronounController : ControllerBase
+    {
+        private readonly IUserPronounRepository _userPronounRepository;
+        public UserPronounController(IUserPronounRepository userPronounRepository)
+        {
+            _userPronounRepository = userPronounRepository;
+        }
+
+        // GET: api/<UserPronounController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userPronounRepository.GetAll());
+        }
+
+        // GET api/<UserPronounController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var userPronoun = _userPronounRepository.GetById(id);
+            if (userPronoun == null)
+            {
+                return NotFound();
+            }
+            return Ok(userPronoun);
+        }
+
+        // POST api/<UserPronounController>
+        [HttpPost]
+        public IActionResult Post(UserPronoun userPronoun)
+        {
+            _userPronounRepository.Add(userPronoun);
+            return CreatedAtAction("Get", new { id = userPronoun.Id }, userPronoun);
+        }
+
+        // PUT api/<UserPronounController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UserPronoun userPronoun)   
+        {
+            if (id != userPronoun.Id)
+            {
+                return BadRequest();
+            }
+            _userPronounRepository.Update(userPronoun);
+            return NoContent();
+        }
+
+        // DELETE api/<UserPronounController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userPronounRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ BookBurrow/Repositories/IUserPronounRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookBurrow.Models;
+
+namespace BookBurrow.Repositories
+{
+    public interface IUserPronounRepository
+    {
+        public List<UserPronoun> GetAll();
+        public UserPronoun GetById(int id);
+        public void Add(UserPronoun userPronoun);
+        public void Update(UserPronoun userPronoun);
+        public void Delete(int id);
+    }
+}
