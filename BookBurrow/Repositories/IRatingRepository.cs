fd86e8fe2065fe6724bb23c73a9088bce9e23c64
Sarah Maginnis <sarahemaginnis@gmail.com>
2022-12-06 20:31:31 -0600

    repository, interface, and controller for Rating to setup basic CRUD functionality

---
+++ BookBurrow/Repositories/IRatingRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookBurrow.Models;
+
+namespace BookBurrow.Repositories
+{
+    public interface IRatingRepository
+    {
+        public List<Rating> GetAll();
+        public Rating GetById(int id);
+        public void Add(Rating rating);
+        public void Update(Rating rating);
+        public void Delete(int id);
+    }
+}
---
+++ BookBurrow/Repositories/RatingRepository.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using BookBurrow.Models;
+using BookBurrow.Utils;
+using Microsoft.Data.SqlClient;
+
+namespace BookBurrow.Repositories
+{
+    public class RatingRepository : BaseRepository, IRatingRepository
+    {
+        public RatingRepository(IConfiguration configuration) : base(configuration) { }
+        public List<Rating> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT id, displayValue
+                            FROM dbo.Rating
+                        ORDER BY displayValue
+                    ";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var ratings = new List<Rating>();
+                        while (reader.Read())
+                        {
+                            ratings.Add(new Rating()
+                            {
+                                Id = DbUtils.GetInt(reader, "id"),
+                                DisplayValue = reader.GetDecimal(reader.GetOrdinal("displayValue")),
+                            });
+                        }
+                        return ratings;
+                    }
+                }
+            }
+        }
+
+        public Rating GetById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT displayValue
+                            FROM dbo.Rating
+                        WHERE Id = @id
+                    ";
+                    DbUtils.AddParameter(cmd, "@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Rating rating = null;
+                        if (reader.Read())
+                        {
+                            rating = new Rating()
+                            {
+                                Id = id,
+                                DisplayValue = reader.GetDecimal(reader.GetOrdinal("displayValue")),
+                            };
+                        }
+                        return rating;
+                    }
+                }
+            }
+        }
+
+        public void Add(Rating rating)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO dbo.Rating (displayValue)
+                        OUTPUT INSERTED.id
+                        VALUES (@displayValue)
+                    ";
+
+                    cmd.Parameters.AddWithValue("@displayValue", rating.DisplayValue);
+
+                    rating.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void Update(Rating rating)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE dbo.Rating
+                            SET displayValue = @displayValue
+                        WHERE Id = @id
+                    ";
+
+                    DbUtils.AddParameter(cmd, "@displayValue", rating.DisplayValue);
+                    DbUtils.AddParameter(cmd, "@id", rating.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM dbo.Rating WHERE Id = @id";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
