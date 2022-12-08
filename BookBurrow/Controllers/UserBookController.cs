9cb82868fbca6d14aea33b09bdedb945c7f5703e
Sarah Maginnis <sarahemaginnis@gmail.com>
2022-12-07 20:39:32 -0600

    first attempt at repository, interface, and controller for UserBook to setup basic CRUD functionality. I am having some errors with my BookStatus (particularly with the POST). See ticket #64 for bug fix

---
+++ BookBurrow/Controllers/UserBookController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using BookBurrow.Repositories;
+using BookBurrow.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookBurrow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserBookController : ControllerBase
+    {
+        private readonly IUserBookRepository _userBookRepository; 
+        public UserBookController(IUserBookRepository userBookRepository)
+        {
+            _userBookRepository = userBookRepository;
+        }
+
+        // GET: api/<UserBookController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userBookRepository.GetAllOrderedByReviewCreatedAt());
+        }
+
+        // GET api/<UserBookController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var userBook = _userBookRepository.GetById(id);
+            if (userBook == null)
+            {
+                return NotFound();
+            }
+            return Ok(userBook);
+        }
+
+        // POST api/<UserBookController>
+        [HttpPost]
+        public IActionResult Post(UserBook userBook)
+        {
+            _userBookRepository.Add(userBook);
+            return CreatedAtAction("Get", new { id = userBook.Id }, userBook);
+        }
+
+        // PUT api/<UserBookController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UserBook userBook)
+        {
+            if (id!= userBook.Id)
+            {
+                return BadRequest();
+            }
+            _userBookRepository.Update(userBook);
+            return NoContent();
+        }
+
+        // DELETE api/<UserBookController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete (int id)
+        {
+            _userBookRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ BookBurrow/Repositories/IUserBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookBurrow.Models;
+
+namespace BookBurrow.Repositories
+{
+    public interface IUserBookRepository
+    {
+        public List<UserBook> GetAllOrderedByReviewCreatedAt();
+        public UserBook GetById(int id);
+        public void Add(UserBook userBook);
+        public void Update(UserBook userBook);
+        public void Delete(int id);
+    }
+}
