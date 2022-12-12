f59c7d73afe355a4876e728ee45be1045667f37d
Sarah Maginnis <sarahemaginnis@gmail.com>
2022-12-12 13:43:12 -0600

    fixed some errors with the registerviewmodel crud functionality - updated repository, interface, and controller to only call back to and user the view model instead of multiple model classes

--- BookBurrow/Repositories/IRegisterViewModelRepository.cs
+++ BookBurrow/Repositories/IRegisterViewModelRepository.cs
@@ -1,14 +1,15 @@
 ﻿using BookBurrow.Models;
+using BookBurrow.ViewModels;
 
 namespace BookBurrow.Repositories
 {
     public interface IRegisterViewModelRepository
     {
-        public List<UserProfile> GetAllUserProfiles();
-        public User GetUserById(int id);
-        public UserProfile GetUserProfileById(int id);
-        public UserRole GetUserRoleById(int id);
-        public void UpdateUserProfile(UserProfile userProfile);
-        public void AddUserRole(UserRole userRole);
+        public RegisterViewModel GetUserById(int id);
+        public List<RegisterViewModel> GetAllUserProfiles();
+        public RegisterViewModel GetUserProfileById(int id);
+        public RegisterViewModel GetUserRoleById(int id);
+        public void UpdateUserProfile(RegisterViewModel registerViewModel);
+        public void AddUserRole(RegisterViewModel registerViewModel);
     }
 }
