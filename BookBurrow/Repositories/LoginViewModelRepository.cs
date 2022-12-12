d0e6b7c9e818bf8bbf2d1727bc5020a02149a39a
Sarah Maginnis <sarahemaginnis@gmail.com>
2022-12-12 13:06:34 -0600

    updated LoginViewModel repository, interface, and controller to fix earlier issues (before, I was using modelobjects instead of view model objects)

--- BookBurrow/Repositories/LoginViewModelRepository.cs
+++ BookBurrow/Repositories/LoginViewModelRepository.cs
@@ -5,13 +5,72 @@ using BookBurrow.Models;
 using BookBurrow.Utils;
 using Microsoft.Data.SqlClient;
 using System.Data.SqlTypes;
+using BookBurrow.ViewModels;
+using CloudinaryDotNet.Actions;
 
 namespace BookBurrow.Repositories
 {
     public class LoginViewModelRepository : BaseRepository, ILoginViewModelRepository
     {
         public LoginViewModelRepository(IConfiguration configuration) : base(configuration) { }
-        public void Add(User user)
+        
+        public List<LoginViewModel> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT u.id AS userId, u.firebaseUID, u.email, u.createdAt AS userCreatedAt, u.updatedAt AS userUpdatedAt,
+                            up.id AS userProfileId, up.userId, up.profileImageUrl, up.firstName, up.lastName, up.handle, up.pronounId, 
+                            up.biography, up.biographyUrl, up.birthday, up.createdAt AS userProfileCreatedAt, up.updatedAt AS userProfileUpdatedAt,
+                            p.pronouns
+                        FROM dbo.[User] u
+                            JOIN dbo.UserProfile up ON u.id = up.userId
+                            JOIN dbo.UserPronoun p ON up.pronounId = p.id
+                        ORDER BY u.createdAt
+                    ";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var usersAndProfiles = new List<LoginViewModel>();
+                        while (reader.Read())
+                        {
+                            usersAndProfiles.Add(new LoginViewModel()
+                            {
+                                User = new User()
+                                {
+                                    Id = DbUtils.GetInt(reader, "userId"),
+                                    FirebaseUID = DbUtils.GetString(reader, "firebaseUID"),
+                                    Email = DbUtils.GetString(reader, "email"),
+                                    CreatedAt = DbUtils.GetDateTime(reader, "userCreatedAt"),
+                                    UpdatedAt = DbUtils.GetDateTime(reader, "userUpdatedAt"),
+                                },
+                                UserProfile = new UserProfile()
+                                {
+                                    Id = DbUtils.GetInt(reader, "userProfileId"),
+                                    UserId = DbUtils.GetInt(reader, "userId"),
+                                    ProfileImageUrl = DbUtils.GetString(reader, "profileImageUrl"),
+                                    FirstName = DbUtils.GetString(reader, "firstName"),
+                                    LastName = DbUtils.GetString(reader, "lastName"),
+                                    Handle = DbUtils.GetString(reader, "handle"),
+                                    PronoundId = DbUtils.GetNullableInt(reader, "pronounId"),
+                                    Biography = DbUtils.GetString(reader, "biography"),
+                                    BiographyUrl = DbUtils.GetString(reader, "biographyUrl"),
+                                    Birthday = DbUtils.GetDateTime(reader, "birthday"),
+                                    CreatedAt = DbUtils.GetDateTime(reader, "userProfileCreatedAt"),
+                                    UpdatedAt = DbUtils.GetDateTime(reader, "userProfileUpdatedAt"),
+                                },
+                            });
+                        }
+                        return usersAndProfiles;
+                    }
+                }
+            }
+        }
+        
+        public void AddUser(LoginViewModel loginViewModel)
         {
             using (var conn = Connection)
             {
@@ -23,17 +82,17 @@ namespace BookBurrow.Repositories
                         OUTPUT INSERTED.id
                         VALUES (@firebaseUID, @email, @createdAt, @updatedAt)
                     ";
-                    DbUtils.AddParameter(cmd, "@firebaseUID", user.FirebaseUID);
-                    DbUtils.AddParameter(cmd, "@email", user.Email);
-                    DbUtils.AddParameter(cmd, "@createdAt", user.CreatedAt);
-                    DbUtils.AddParameter(cmd, "@updatedAt", user.UpdatedAt);
+                    DbUtils.AddParameter(cmd, "@firebaseUID", loginViewModel.User.FirebaseUID);
+                    DbUtils.AddParameter(cmd, "@email", loginViewModel.User.Email);
+                    DbUtils.AddParameter(cmd, "@createdAt", loginViewModel.User.CreatedAt);
+                    DbUtils.AddParameter(cmd, "@updatedAt", loginViewModel.User.UpdatedAt);
 
-                    user.Id = (int)cmd.ExecuteScalar();
+                    loginViewModel.User.Id = (int)cmd.ExecuteScalar();
                 }
             }
         }
 
-        public void Add(UserProfile userProfile)
+        public void AddUserProfile(LoginViewModel loginViewModel)
         {
             using (var conn = Connection)
             {
@@ -45,19 +104,19 @@ namespace BookBurrow.Repositories
                         OUTPUT INSERTED.id
                         VALUES (@userId, @profileImageUrl, @firstName, @lastName, @handle, @pronounId, @biography, @biographyUrl, @birthday, @createdAt, @updatedAt)
                     ";
-                    DbUtils.AddParameter(cmd, "@userId", userProfile.UserId);
-                    DbUtils.AddParameter(cmd, "@profileImageUrl", userProfile.ProfileImageUrl);
-                    DbUtils.AddParameter(cmd, "@firstName", userProfile.FirstName);
-                    DbUtils.AddParameter(cmd, "@lastName", userProfile.LastName);
-                    DbUtils.AddParameter(cmd, "@handle", userProfile.Handle);
-                    DbUtils.AddParameter(cmd, "@pronounId", userProfile.PronoundId);
-                    DbUtils.AddParameter(cmd, "@biography", userProfile.Biography);
-                    DbUtils.AddParameter(cmd, "@biographyUrl", userProfile.BiographyUrl);
-                    DbUtils.AddParameter(cmd, "@birthday", userProfile.Birthday);
-                    DbUtils.AddParameter(cmd, "@createdAt", userProfile.CreatedAt);
-                    DbUtils.AddParameter(cmd, "@updatedAt", userProfile.UpdatedAt);
+                    DbUtils.AddParameter(cmd, "@userId", loginViewModel.UserProfile.UserId);
+                    DbUtils.AddParameter(cmd, "@profileImageUrl", loginViewModel.UserProfile.ProfileImageUrl);
+                    DbUtils.AddParameter(cmd, "@firstName", loginViewModel.UserProfile.FirstName);
+                    DbUtils.AddParameter(cmd, "@lastName", loginViewModel.UserProfile.LastName);
+                    DbUtils.AddParameter(cmd, "@handle", loginViewModel.UserProfile.Handle);
+                    DbUtils.AddParameter(cmd, "@pronounId", loginViewModel.UserProfile.PronoundId);
+                    DbUtils.AddParameter(cmd, "@biography", loginViewModel.UserProfile.Biography);
+                    DbUtils.AddParameter(cmd, "@biographyUrl", loginViewModel.UserProfile.BiographyUrl);
+                    DbUtils.AddParameter(cmd, "@birthday", loginViewModel.UserProfile.Birthday);
+                    DbUtils.AddParameter(cmd, "@createdAt", loginViewModel.UserProfile.CreatedAt);
+                    DbUtils.AddParameter(cmd, "@updatedAt", loginViewModel.UserProfile.UpdatedAt);
 
-                    userProfile.Id = (int)cmd.ExecuteScalar();
+                    loginViewModel.UserProfile.Id = (int)cmd.ExecuteScalar();
                 }
             }
         }
