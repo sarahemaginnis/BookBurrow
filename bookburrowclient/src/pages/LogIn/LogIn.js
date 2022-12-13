37353527869d6e1161dc3c9cd7babf17c067e565
Sarah Maginnis <sarahemaginnis@gmail.com>
2022-12-13 12:19:44 -0600

    some minor stylilng of login view; included logo

--- bookburrowclient/src/pages/LogIn/LogIn.js
+++ bookburrowclient/src/pages/LogIn/LogIn.js
@@ -1,31 +1,36 @@
+import './LogIn.css';
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { signIntoFirebase } from "../../utils/auth";
-import MyImage from '.BookBurrowLogo.png';
+import MyImage from "./BookBurrowLogo.png";
 
 export default function LogIn() {
   return (
     <div className="text-center mt-5">
       <Container>
-        <Row>
-          <Col>
-            <h1>Book Burrow</h1>
-            <h2>Find your fandom</h2>
-            <h2>find your friends.</h2>
-            <button
-              type="button"
-              className="btn btn-success"
-              onClick={signIntoFirebase}
-            >
-              Enter
-            </button>
-          </Col>
-          <Col>
-            <div>
-              <img className="image__login" src={MyImage} alt="Book Burrow Logo" />
-            </div>
-          </Col>
-        </Row>
+          <Row>
+            <Col>
+              <h1>Book Burrow</h1>
+              <h2>Find your fandom</h2>
+              <h2>find your friends.</h2>
+              <Button
+                type="button"
+                className="btn__btn-primary"
+                onClick={signIntoFirebase}
+              >
+                Enter
+              </Button>
+            </Col>
+            <Col>
+              <div>
+                <img
+                  className="image__login"
+                  src={MyImage}
+                  alt="Book Burrow Logo"
+                />
+              </div>
+            </Col>
+          </Row>
       </Container>
     </div>
   );
